274fa00c4eaf4c5aff073a745ee8e6df68059bfe
cesiedrn <duranaprinces35@gmail.com>
2025-04-30 11:35:05 +0800

    css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './pages/About';
 import Education from './pages/Education';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import './App.css';
+
 
 function App() {
   return (
